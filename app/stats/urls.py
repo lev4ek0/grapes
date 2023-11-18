7ca03ab742a2418d04faf6710127873bafab2456
lev <leva22.08.01@inbox.ru>
2023-11-18 23:24:12 +0300

    heatmap

--- app/stats/urls.py
+++ app/stats/urls.py
@@ -4,6 +4,7 @@ from stats import views
 
 urlpatterns = [
     path("illnesses", views.IllnessListAPIView.as_view()),
+    path("heatmap/<slug:date>", views.HeatMapAPIView.as_view()),
     path("forecast/worst/<slug:date>", views.WorstForecastAPIView.as_view()),
     path("forecast/map/<slug:date>", views.ForecastMapAPIView.as_view()),
 ]
--- app/stats/views.py
+++ app/stats/views.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
 from copy import deepcopy
+import datetime
+import math
 
 from rest_framework.request import Request
 from rest_framework.views import APIView, Response
@@ -25,6 +28,7 @@ def get_query_params(func):
             regions = Region.objects.all()
         kwargs["regions"] = regions
         kwargs["limit"] = int(request.query_params.get("limit", 5))
+        kwargs["mode"] = request.query_params.get("mode", "percent")
         illnesses_list = request.query_params.getlist("illness_ids")
         if illnesses_list:
             illnesses = Illness.objects.filter(id__in=illnesses_list)
@@ -35,15 +39,58 @@ def get_query_params(func):
     return wrapper
 
 
-class WorstForecastAPIView(APIView):
+class HeatMapAPIView(APIView):
     @get_query_params
-    def get(self, request: Request, date: str, regions: list[Region], limit: int, illnesses: set[str]):
+    def get(self, request: Request, date: str, regions: list[Region], illnesses: set[str], mode: str, **kwargs):
         serialized_date = DateSerializer(data={"date": date})
         if not serialized_date.is_valid():
             return Response(status=404)
         forecasts = []
+        date = datetime.datetime.strptime(serialized_date.data["date"], "%Y-%m-%d")
+        output = {
+            "dates": [],
+            "data": [],
+            "illnesses": [],
+        }
+        for day_index in range(-6, 4):
+            cur_date = date + datetime.timedelta(days=day_index)
+            illness_map = {illness: 0 for illness in illnesses}
+            for region in regions:
+                forecast_result = deepcopy(forecast(cur_date, region.pk))
+                for illness in list(filter(lambda x: x["name"] in illnesses, forecast_result["illnesses"])):
+                    if mode == 'percent':
+                        illness_map[illness["name"]] += illness["percent"]
+                    else:
+                        illness_map[illness["name"]] += 1
+            forecasts.append(illness_map)
+            output["dates"].append(cur_date.strftime("%d-%m-%Y"))
+        
+        output["illnesses"] = sorted(illness_map.keys(), key=lambda x: x[0])
+        minimum = math.inf
+        maximum = 0
+        for x in range(len(forecasts)):
+            for y, (illness, value) in enumerate(sorted(forecasts[x].items(), key=lambda x: x[0])):
+                if mode == 'percent':
+                    val = round(value/len(regions), 2)
+                    output["data"].append([x, y, val if value else '-'])
+                else:
+                    val = value
+                    output["data"].append([x, y, val if value else '-'])
+                maximum, minimum = max(maximum, val), min(minimum, val)
+        output["max"], output["min"] = maximum, minimum
+        return Response(output)
+
+
+class WorstForecastAPIView(APIView):
+    @get_query_params
+    def get(self, request: Request, date: str, regions: list[Region], limit: int, illnesses: set[str], **kwargs):
+        serialized_date = DateSerializer(data={"date": date})
+        if not serialized_date.is_valid():
+            return Response(status=404)
+        forecasts = []
+        date = datetime.strptime(serialized_date, "%d-%m-%Y")
         for region in regions:
-            forecast_result = deepcopy(forecast(serialized_date.data["date"], region.pk))
+            forecast_result = deepcopy(forecast(date, region.pk))
             forecast_result["region"] = RegionSerializer(region).data
             forecast_result["illnesses"] = list(filter(lambda x: x["name"] in illnesses, forecast_result["illnesses"]))
             forecasts.append(forecast_result)
@@ -58,8 +105,9 @@ class ForecastMapAPIView(APIView):
         if not serialized_date.is_valid():
             return Response(status=404)
         forecasts = []
+        date = datetime.strptime(serialized_date, "%d-%m-%Y")
         for region in regions:
-            forecast_result = deepcopy(forecast(serialized_date.data["date"], region.pk))
+            forecast_result = deepcopy(forecast(date, region.pk))
             forecast_result["region"] = RegionSerializer(region).data
             forecast_result["illnesses"] = list(filter(lambda x: x["name"] in illnesses, forecast_result["illnesses"]))
             forecasts.append(forecast_result)
